65a8ef6219da08d47a9491b1591095f19b5374e7
brunoisouza <lubru70@hotmail.com>
2024-02-15 19:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ Pato.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Zoo
+{
+    //Criação da Classe
+    internal class Pato
+    {
+        //Atributos da classe
+        public string nome;
+        public decimal peso { get; }
+        public decimal altura { get; }
+        public char genero { get; }
+        public string especie { get; set; }
+        public bool cio { get; }
+
+        private DateTime data_nascimento;
+
+        private int nivel_medo;
+
+        // Metodo Construtor da Classe
+        public Pato(decimal peso, decimal altura, char genero, string especie, bool cio)
+        {
+            this.peso = peso;
+            this.altura = altura;
+            this.genero = genero;
+            this.especie = especie;
+            this.cio = cio;
+
+            this.data_nascimento = DateTime.Now;
+            this.nivel_medo = 0;
+
+            this.Grasnar();
+        }
+        
+        //Como a classe vai se comportar ao se transformar em texto
+        public override string ToString()
+        {
+            return $"Olá, eu sou {this.nome} e tenho {this.getTempoVida()}. Atualmente, " +
+                $"peso {this.peso} e tenho {this.altura} de tamanho. Ahhh, " +
+                $"eu {(this.cio ? "" : "não")} estou no cio.";
+        }
+        
+        //Função para Pegar o tempo de vida
+        public string getTempoVida()
+        {
+            TimeSpan tempo_vida = DateTime.Now - this.data_nascimento;
+            return tempo_vida.ToString("c");
+        }
+        
+        //Função para o pato grasnar
+        public void Grasnar()
+        {
+            if (nivel_medo ==  0)
+            {
+                MessageBox.Show("Quack");
+            } 
+            else if (nivel_medo > 0)
+            {
+                MessageBox.Show("Quack!");
+            }
+            else if (nivel_medo > 5)
+            {
+                MessageBox.Show("Quaaaaaaaaaaaaaaaaaack!");
+            }
+        }
+    }
+}
---
+++ Patolandia.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zoo
+{
+    public partial class Patolandia : Form
+    {
+        // Cria uma lista vazia de patos
+        List<Pato> patos = new List<Pato>();
+
+        public Patolandia()
+        {
+            InitializeComponent();
+        }
+
+        //Botão para criar o pato
+        private void btnCriarPato_Click(object sender, EventArgs e)
+        {
+            //Pegar os valores da tela e atribuir as variáveis
+            string nome = txtNome.Text;
+            string especie = txtEspecie.Text;
+            char genero = cmbGenero.SelectedIndex == 0 ? 'M' : 'F';
+            decimal peso = numPeso.Value;
+            decimal altura = numAltura.Value;
+            // Verifica se a caixa está marcada ou não
+            bool cio = ckbEstaCio.Checked;
+
+            //Criar o pato novo
+            Pato novo_pato = new Pato(peso, altura, genero, especie, cio);
+            novo_pato.nome = nome;
+
+            //Adicionar a lista de patos
+            patos.Add(novo_pato);
+        }
+
+        //Botão para ver o pato
+        private void btnVerPato_Click(object sender, EventArgs e)
+        {
+            //Seta o pato a partir do numero selecionado
+            Pato pato = patos[(int)numNumeroPato.Value];
+            txtVisor.Text = pato.ToString();
+        }
+    }
+
+}
